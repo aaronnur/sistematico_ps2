8cb21d274d6f5e36e672f73ce62851d14b3c722a
aaronnur <145123249+aaronnur@users.noreply.github.com>
2026-04-09 01:42:54 +0000

    não finalizado

---
+++ empresas-estudantes-vagas/src/main/java/com/example/empresas_estudantes_vagas/EmpresasEstudantesVagasApplication.java
@@ -0,0 +1,11 @@
+package com.example.empresasestudantesvagas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpresasEstudantesVagasApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EmpresasEstudantesVagasApplication.class, args);
+    }
+}
---
+++ empresas-estudantes-vagas/src/test/java/com/example/empresas_estudantes_vagas/EmpresasEstudantesVagasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.empresas_estudantes_vagas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresasEstudantesVagasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
